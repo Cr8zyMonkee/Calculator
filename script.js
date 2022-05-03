983ece0c8f52144b5743c64b0d89c95d6086ca28
Cr8zyMonkee <alexanderjnunez98@gmail.com>
2022-05-02 20:17:35 -0700

    Creates JavaScript file and basic operator functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(add1, add2) {
+    return add1 + add2;
+}
+
+function subtract(sub1, sub2) {
+    return sub1 - sub2;
+}
+
+function multiply(mul1, mul2) {
+    return mul1 * mul2;
+}
+
+function divide(div1, div2) {
+    return div1 / div2;
+}
