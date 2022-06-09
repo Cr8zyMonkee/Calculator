59d6ab1a7b71f4d3d2f1b2c392d41567564df338
Alex <alexanderjnunez98@gmail.com>
2022-06-08 17:58:54 -0700

    fixes bugs caused by styling

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//functions
+
 function equal() {
     num2 = Number(tempNum);
     equals(num1, num2, tempOp);
@@ -86,6 +88,30 @@ function undo() {
     document.getElementById('display').innerHTML = displayCurrent;
 }
 
+function numPressed(num) {
+    if (num === 'n0') {
+        return '0';
+    } else if (num === 'n1') {
+        return '1';
+    } else if (num === 'n2') {
+        return '2';
+    } else if (num === 'n3') {
+        return '3';
+    } else if (num === 'n4') {
+        return '4';
+    } else if (num === 'n5') {
+        return '5';
+    } else if (num === 'n6') {
+        return '6';
+    } else if (num === 'n7') {
+        return '7';
+    } else if (num === 'n8') {
+        return '8';
+    } else if (num === 'n9') {
+        return '9';
+    }
+}
+
 function operate() {
 
     if (tempClass === 'operators') {
@@ -105,6 +131,7 @@ function operate() {
     }
 }
 
+// Variables
 
 let displayCurrent = '0';
 let firstNum = '';
@@ -125,7 +152,7 @@ let subSym = '-';
 let multSym = 'x';
 let divSym = '÷';
 
-
+// event listeners
 document.getElementById('display').innerHTML = displayCurrent;
 
 const displayBtn = document.querySelectorAll('button');
@@ -154,9 +181,9 @@ addBtn.addEventListener('click', () => {
     addition = true;
     operatorPressed = true;
     displayCurrent = addSym;
-    if(operatorPressed === true){
+    if (operatorPressed === true) {
         return;
-    }else{
+    } else {
         document.getElementById('display').innerHTML = displayCurrent;
     }
 })
@@ -167,9 +194,9 @@ subBtn.addEventListener('click', () => {
     subtraction = true;
     operatorPressed = true;
     displayCurrent = subSym;
-    if(operatorPressed === true){
+    if (operatorPressed === true) {
         return;
-    }else{
+    } else {
         document.getElementById('display').innerHTML = displayCurrent;
     }
 })
@@ -180,9 +207,9 @@ multBtn.addEventListener('click', () => {
     multi = true;
     operatorPressed = true;
     displayCurrent = multSym;
-    if(operatorPressed === true){
+    if (operatorPressed === true) {
         return;
-    }else{
+    } else {
         document.getElementById('display').innerHTML = displayCurrent;
     }
 })
@@ -193,9 +220,9 @@ divBtn.addEventListener('click', () => {
     divi = true;
     operatorPressed = true;
     displayCurrent = divSym;
-    if(operatorPressed === true){
+    if (operatorPressed === true) {
         return;
-    }else{
+    } else {
         document.getElementById('display').innerHTML = displayCurrent;
     }
 })
@@ -227,8 +254,8 @@ equalBtn.addEventListener('click', () => {
 const numBtn = document.querySelectorAll('.numBtns');
 numBtn.forEach(function (num) {
     num.addEventListener('click', () => {
-        let tempNum = num.getAttribute('id');
-
+        let numID = num.getAttribute('id');
+        let tempNum = numPressed(numID);
         if (operatorPressed === false) {
             firstNumArr.push(tempNum);
             firstNum = Number(firstNumArr.join(''));
@@ -263,3 +290,251 @@ decimalBtn.addEventListener('click', () => {
         document.getElementById('display').innerHTML = displayCurrent;
     }
 })
+
+const deleteBtn = document.querySelector('#undo');
+deleteBtn.addEventListener('click', () => {
+    undo();
+})
+
+//keyboard support
+
+document.addEventListener('keydown', (e) => {
+    if (e.code === 'Backspace') {
+        undo();
+    }
+    else if (e.code === 'Enter' || e.code === '') {
+        if ((firstNum != '' && secondNum != '') &&
+            operationCompleted === false) {
+            equals(Number(firstNum), Number(secondNum));
+            operationCompleted = true;
+            addition = false;
+            subtraction = false;
+            divi = false;
+            multi = false;
+            operatorPressed = false;
+            displayCurrent = Math.round(Number(total) * 100) / 100;
+            document.getElementById('display').innerHTML = displayCurrent;
+            total = 0;
+        }
+        return;
+    }
+    else if (e.code === 'Period' || e.code === 'NumpadDecimal') {
+        if (operatorPressed === false && firstDecimalPressed === false) {
+            firstNumArr.push('.');
+            firstDecimalPressed = true;
+            firstNum = firstNumArr.join('');
+            displayCurrent = firstNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+        else if (operatorPressed === true && secondDecimalPressed === false) {
+            secondNumArr.push(".");
+            secondDecimalPressed = true;
+            secondNum = secondNumArr.join('');
+            displayCurrent = secondNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+    else if (e.code === 'Delete') {
+        clear();
+    }
+    else if (e.code === 'NumpadMultiply') {
+        multi = true;
+        operatorPressed = true;
+        displayCurrent = multSym;
+        if (operatorPressed === true) {
+            return;
+        } else {
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+    else if (e.code === 'NumpadDivide') {
+        divi = true;
+        operatorPressed = true;
+        displayCurrent = divSym;
+        if (operatorPressed === true) {
+            return;
+        } else {
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+    else if (e.code === 'NumpadSubtract') {
+        subtraction = true;
+        operatorPressed = true;
+        displayCurrent = subSym;
+        if (operatorPressed === true) {
+            return;
+        } else {
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+    else if (e.code === 'NumpadAdd') {
+        addition = true;
+        operatorPressed = true;
+        displayCurrent = addSym;
+        if (operatorPressed === true) {
+            return;
+        } else {
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+    else if (e.code === 'Numpad0' || e.code === 'Digit0') {
+        let tempNum = '0';
+
+        if (operatorPressed === false) {
+            firstNumArr.push(tempNum);
+            firstNum = Number(firstNumArr.join(''));
+            displayCurrent = firstNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+        else if (operatorPressed === true) {
+            secondNumArr.push(tempNum);
+            secondNum = Number(secondNumArr.join(''));
+            displayCurrent = secondNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+    else if (e.code === 'Numpad1' || e.code === 'Digit1') {
+        let tempNum = '1';
+
+        if (operatorPressed === false) {
+            firstNumArr.push(tempNum);
+            firstNum = Number(firstNumArr.join(''));
+            displayCurrent = firstNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+        else if (operatorPressed === true) {
+            secondNumArr.push(tempNum);
+            secondNum = Number(secondNumArr.join(''));
+            displayCurrent = secondNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+    else if (e.code === 'Numpad2' || e.code === 'Digit2') {
+        let tempNum = '2';
+
+        if (operatorPressed === false) {
+            firstNumArr.push(tempNum);
+            firstNum = Number(firstNumArr.join(''));
+            displayCurrent = firstNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+        else if (operatorPressed === true) {
+            secondNumArr.push(tempNum);
+            secondNum = Number(secondNumArr.join(''));
+            displayCurrent = secondNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+    else if (e.code === 'Numpad3' || e.code === 'Digit3') {
+        let tempNum = '3';
+
+        if (operatorPressed === false) {
+            firstNumArr.push(tempNum);
+            firstNum = Number(firstNumArr.join(''));
+            displayCurrent = firstNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+        else if (operatorPressed === true) {
+            secondNumArr.push(tempNum);
+            secondNum = Number(secondNumArr.join(''));
+            displayCurrent = secondNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+    else if (e.code === 'Numpad4' || e.code === 'Digit4') {
+        let tempNum = '4';
+
+        if (operatorPressed === false) {
+            firstNumArr.push(tempNum);
+            firstNum = Number(firstNumArr.join(''));
+            displayCurrent = firstNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+        else if (operatorPressed === true) {
+            secondNumArr.push(tempNum);
+            secondNum = Number(secondNumArr.join(''));
+            displayCurrent = secondNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+    else if (e.code === 'Numpad5' || e.code === 'Digit5') {
+        let tempNum = '5';
+
+        if (operatorPressed === false) {
+            firstNumArr.push(tempNum);
+            firstNum = Number(firstNumArr.join(''));
+            displayCurrent = firstNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+        else if (operatorPressed === true) {
+            secondNumArr.push(tempNum);
+            secondNum = Number(secondNumArr.join(''));
+            displayCurrent = secondNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+    else if (e.code === 'Numpad6' || e.code === 'Digit6') {
+        let tempNum = '6';
+
+        if (operatorPressed === false) {
+            firstNumArr.push(tempNum);
+            firstNum = Number(firstNumArr.join(''));
+            displayCurrent = firstNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+        else if (operatorPressed === true) {
+            secondNumArr.push(tempNum);
+            secondNum = Number(secondNumArr.join(''));
+            displayCurrent = secondNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+    else if (e.code === 'Numpad7' || e.code === 'Digit7') {
+        let tempNum = '7';
+
+        if (operatorPressed === false) {
+            firstNumArr.push(tempNum);
+            firstNum = Number(firstNumArr.join(''));
+            displayCurrent = firstNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+        else if (operatorPressed === true) {
+            secondNumArr.push(tempNum);
+            secondNum = Number(secondNumArr.join(''));
+            displayCurrent = secondNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+    else if (e.code === 'Numpad8' || e.code === 'Digit8') {
+        let tempNum = '8';
+
+        if (operatorPressed === false) {
+            firstNumArr.push(tempNum);
+            firstNum = Number(firstNumArr.join(''));
+            displayCurrent = firstNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+        else if (operatorPressed === true) {
+            secondNumArr.push(tempNum);
+            secondNum = Number(secondNumArr.join(''));
+            displayCurrent = secondNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+    else if (e.code === 'Numpad9' || e.code === 'Digit9') {
+        let tempNum = '9';
+
+        if (operatorPressed === false) {
+            firstNumArr.push(tempNum);
+            firstNum = Number(firstNumArr.join(''));
+            displayCurrent = firstNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+        else if (operatorPressed === true) {
+            secondNumArr.push(tempNum);
+            secondNum = Number(secondNumArr.join(''));
+            displayCurrent = secondNum;
+            document.getElementById('display').innerHTML = displayCurrent;
+        }
+    }
+});
